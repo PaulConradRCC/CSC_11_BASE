330d380c570b3478da6470b8a9f713aa0758e042
Paul J Conrad <pjc@pauljconrad.net>
2023-10-20 19:14:23 -0700

    Updates in aarch64

--- aarch64/add/add_4/add_4.s
+++ aarch64/add/add_4/add_4.s
@@ -1,77 +1,89 @@
-// bare bones source file
 .global main
 
-.align 4
+.align 8
 .section .rodata
 prompt:		.asciz "Enter two unsigned 32 bit integers: "
 scan_str:	.asciz	"%u %u"
 format_string:	.asciz "%u + %u = %u\n"
-format_str2:	.asciz "%u - %u = %u\n"
-format_str3:	.asciz "%u * %u = %u\n"
-format_str4:	.asciz "%u * %u = "
+format_string2:	.asciz "%u - %u = %u\n"
+format_string3:	.asciz "%u * %u = %u\n"
+format_string4:	.asciz "%u * %u = "
 format_str4_2:	.asciz "%llu (umull)\n"
 
-.align 4
+.align 8
 .section .data
 val1:		.word	0
 val2:		.word	0
 
-.align 4
+.align 8
 .text
 main:
-	push {lr}	// save lr (return address from main) on stack for end of main
+	stp fp, lr, [sp, -16]!
+
+	// x19 is pointer to val1, x20 is pointer to val2
+	adrp x19, val1
+	add x19, x19, :lo12: val1
+	adrp x20, val2
+	add x20, x20, :lo12: val2
+
 	// assembly program here
 	// prompt user for two 32 bit integers
-	ldr r0, =prompt
+	adrp x0, prompt
+	add x0, x0, :lo12: prompt
 	bl printf
 
 	// read in the two 32 bit integers from the keyboard via scanf
-	ldr r0, =scan_str
-	ldr r1, =val1
-	ldr r2, =val2
+	adrp x0, scan_str
+	add x0, x0, :lo12: scan_str
+	mov x1, x19
+	mov x2, x20
 	bl scanf
 
 	// load from memory, the values for val1 and val2,
-	// storing the values in r4 and r5, respectively
-	ldr r0, =val1
-	ldr r1, =val2
-	ldr r4, [r0]
-	ldr r5, [r1]
+	// storing the values in x21 and x22, respectively
+	ldr x21, [x19]
+	ldr x22, [x20]
 
-	adds r6, r4, r5	// r6 = r4 + r5, s=update cpsr
-	subs r7, r4, r5  // r7 = r4 - r5, s=update cpsr
-	muls r8, r4, r5  // r8 = r4 * r5, s=update cpsr
-	umull r9, r10, r4, r5 // r9 = r4 * r5 (31:0), r10 = r4 * r5 (63:32)
+	adds x23, x21, x22	// x23 = x21 + x22, s=update cpsr
+	subs x24, x21, x22  	// x24 = x21 - x22, s=update cpsr
+	mul x25, x21, x22  	// x25 = x21 * x22, s=update cpsr
+	umull x26, w21, w22 	// x26 = x21 * x22 (31:0), x27 = x21 * x22 (63:32)??????
 
-	ldr r0, =format_string	// r0 = &format_string
-	mov r1, r4	// r1 = r4
-	mov r2, r5	// r2 = r5
-	mov r3, r6	// r3 = r6
+	adrp x0, format_string	// x0 = &format_string
+	add x0, x0, :lo12: format_string
+	mov x1, x21	// x1 = x21
+	mov x2, x22	// x2 = x22
+	mov x3, x23	// x3 = x23
 	bl printf	// call printf with bl (branch with link)
 
-	ldr r0, =format_str2	// r0 = &format_str2
-	mov r1, r4	// r1 = r4
-	mov r2, r5	// r2 = r5
-	mov r3, r7	// r3 = r7
+	adrp x0, format_string2	// x0 = &format_string
+	add x0, x0, :lo12: format_string2
+	mov x1, x21	// x1 = x21
+	mov x2, x22	// x2 = x22
+	mov x3, x24	// x3 = x24
 	bl printf	// call printf with bl (branch with link)
 
-	ldr r0, =format_str3	// r0 = &format_str3
-	mov r1, r4	// r1 = r4
-	mov r2, r5	// r2 = r5
-	mov r3, r8	// r3 = r8
+	adrp x0, format_string3	// x0 = &format_string
+	add x0, x0, :lo12: format_string3
+	mov x1, x21	// x1 = x21
+	mov x2, x22	// x2 = x22
+	mov x3, x25	// x3 = x25
 	bl printf	// call printf with bl (branch with link)
 
-	ldr r0, =format_str4	// r0 = &format_str4
-	mov r1, r4	// r1 = r4
-	mov r2, r5	// r2 = r5
-	bl printf
-
-	ldr r0, =format_str4_2
-	mov r1, r9
-	mov r2, r10	// r3 = r9
+	adrp x0, format_string4	// x0 = &format_string
+	add x0, x0, :lo12: format_string4
+	mov x1, x21	// x1 = x21
+	mov x2, x22	// x2 = x22
+	mov x3, x26	// x3 = x26
 	bl printf	// call printf with bl (branch with link)
 
-	mov r0, #0 // return code for your program (must be 8 bits)
-	pop {pc}
-	//mov pc, lr
+	adrp x0, format_str4_2
+	add x0, x0, :lo12: format_str4_2
+	mov x1, x11
+	mov x2, x22	// x3 = x20
+	mov x3, x27
+	bl printf	// call printf with bl (branch with link)
 
+	mov w0, #0 // return code for your program (must be 8 bits)
+
+	ldp fp, lr, [sp], 16
