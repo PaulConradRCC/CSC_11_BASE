9e9b0ad4076d00763f79dded9192126e6b6ee1ce
pauljconrad <pjc@pauljconrad.net>
2024-10-18 13:22:40 -0700

    Update compare.s to use w registers with .word labels

--- aarch64/comparisons/compare/compare.s
+++ aarch64/comparisons/compare/compare.s
@@ -1,4 +1,3 @@
-// bare bones source file
 .global main
 
 .align 8
@@ -21,24 +20,26 @@ adr_val2:	.dword	val2
 main:
 	stp fp, lr, [sp, -16]!		// save lr (return address from main) on stack for end of main
 	// assembly program here
+	ldr x18, adr_val1
+	ldr x19, adr_val2
+
 	// prompt user for two 32 bit integers
 	ldr x0, adr_prompt
 	bl printf
 
 	// read in the two 32 bit integers from the keyboard via scanf
 	ldr x0, adr_scan_str
-	ldr x1, adr_val1
-	ldr x2, adr_val2
+	mov x1, x18
+	mov x2, x19
 	bl scanf
 
 	// load from memory, the values for val1 and val2,
-	// storing the values in r4 and r5, respectively
-	ldr x0, adr_val1
-	ldr x1, adr_val2
-	ldr x4, [x0]
-	ldr x5, [x1]
+	// storing the values in w4 and w5, respectively
+	// note that w registers are used to store variables declared as words
+	ldr w4, [x18]
+	ldr w5, [x19]
 
-	cmp x4, x5 // use debugger see what happens to CPSR...
+	cmp w4, w5 // use debugger see what happens to CPSR...
 bkpt:	mov x0, x0 // bogus instruction for our breakpoint...
 
 	mov w0, #0 // return code for your program
