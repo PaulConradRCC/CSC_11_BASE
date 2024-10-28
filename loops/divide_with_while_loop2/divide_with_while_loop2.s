366e0a033dfb296e006fa94d8ab719563d36c9ef
pauljconrad <pjc@pauljconrad.net>
2024-10-28 10:17:18 -0700

    Fix signed value issues.

--- loops/divide_with_while_loop2/divide_with_while_loop2.s
+++ loops/divide_with_while_loop2/divide_with_while_loop2.s
@@ -38,16 +38,61 @@ main: 	push {lr} // save link register, this is one of many ways this can be don
 	// while loop to compute: R4 / R5, and R4 % R5
 	// have result of the division stored in r0, and modulus in r1.
 
+	// check to see if r4 is negative
+	mov r6, #0		// r6 = 0 => r6 is false, sign for r4 (0=positive, -1=negative)
+	mov r7, #0		// r7 = 0 => r7 is false, sign for r5 (0=positive, -1=negative)
+	mov r8, #0		// r8 used for dividend and divisor states
+				// dividend    divisor   r8 state    r5 value     r4 value	r0 = r4 / r5     r1 = r4 % r5
+				// ------------------------------------------------------------------------------------------
+				//  positive   positive  state 0    +divisor     +dividend      no sign changes needed
+				//  positive   negative  state 1    -divisor     +dividend	r0 = -r0         r1 = r1 - r5
+				//  negative   positive  state 2    +divisor     -dividend      r0 = -r0         r1 = r5 - r1
+			        //  negative   negative  state 3    -divisor     -divisor       no change        r1 = -r1
+	cmp r5, #0		// r5 < 0?
+	movlt r6, #-1		// r6 = -1 => r6 is true, we have a negative value
+	cmp r4, #0
+	movlt r7, #-1
+
+	// set state of r8 reg based on above table
+	cmp r6, #0
+	bne r6_negative
+	cmp r7, #0
+	bne r6_positive_r7_negative
+	// next line only executes when r6==0 AND r7==0
+	mov r8, #0		// r8 = do no changes state
+	b end_state_set
+r6_positive_r7_negative:
+	// next line only executes when r6==0 AND r7==-1
+	mov r8, #2		// r8 = state 2 (negative / positive)
+	b end_state_set
+r6_negative:
+	cmp r7, #0
+	bne r6_negative_r7_negative
+	// next line only executes when r6==-1 AND r7==0
+	mov r8, #1		// r8 = state 1 (positive / negative)
+	b end_state_set
+r6_negative_r7_negative:
+	// next line only executes when r6==-1 AND r7==-1
+	mov r8, #3		// r8 = state 3 (negative / negative)
+end_state_set:
+
+	// set r4 and r5 as needed based on r8 state
+	cmp r8, #1
+	muleq r5, r6, r5	// r5 = r6 * r5, r6 should be -1, simply flip r5's sign to positive for now
+
+	cmp r8, #2
+	muleq r4, r7, r4	// r4 = r7 * r4, r7 should be -1, simply flip r4's sign to positive for now
+
+	cmp r8, #3		// this is essentially state 1 and state 2 for (negative / negative)
+	muleq r4, r6, r4	// see for state 1
+	muleq r5, r7, r5	// see for state 2
+
+	// when we reach this point, r4 and r5 should be positive values and r8 should be set to the appropriate state
+	// for the signs of the dividend and divisor, so carry out the division algorithm via successive subtraction
 	// while loop division alg:
 	mov r2, r5
 	mov r1, r4
 	mov r0, #0
-
-	// check to see if r4 is negative
-	mov r6, #0		// r6 = 0 => r6 is false, number hasn't been checked yet
-	cmp r4, #0		// r4 < 0?
-	movlt r6, #-1		// r6 = -1 => r6 is true, we have a negative value
-	mullt r4, r6, r4	// r4 = r6 * r4
 
 while_r1_ge_r2:
 	cmp r1, r2
@@ -58,9 +103,22 @@ while_r1_ge_r2:
 	b while_r1_ge_r2
 end_while_r1_ge_r2:
 
-	cmp r6, #-1		// r6 == -1?
-	muleq r4, r6, r4	// r4 = r6 * r4
-	muleq r0, r6, r0	// r0 = r6 * r0
+	// adjust r0 and r1 based on the state of r8 (see the table about the state of r8 above for reference
+	// set r4 and r5 back to their orginal signs as needed based on r8 state
+	cmp r8, #1
+	muleq r0, r6, r0	// r0 = r6 * r0, r6 should be -1
+	subeq r1, r1, r5	// r1 = r1 - r5
+	muleq r5, r6, r5	// r5 = r6 * r5, r6 should be -1, simply flip r5's sign back to negative
+
+	cmp r8, #2
+	muleq r0, r7, r0	// r0 = r7 * r0, r7 should be -1
+	subeq r1, r5, r1	// r1 = r5 - r1
+	muleq r4, r7, r4	// r4 = r7 * r4, r7 should be -1, simply flip r4's sign back to negative
+
+	cmp r8, #3		// this is essentially state 1 and state 2 for (negative / negative)
+	muleq r1, r6, r1	// r1 = r6 * r1, r6 should be negative (note: could've used r7 as it should be negative as well)
+	muleq r4, r6, r4	// see for state 1
+	muleq r5, r7, r5	// see for state 2
 
 	mov r6, r0	// r6 is a copy of r0, which is r4 / r5
 	mov r7, r1	// r7 is a copy of r1, which is r4 % r5
